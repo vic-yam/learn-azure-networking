2c311824fc7cf24b4e6687bb313d0d8dc5290284
Victor Yampolsky <Victor.Yampolsky@cust2mate.com>
2026-03-06 14:32:29 +0200

    Add Azure VNet setup scripts and Terraform configuration
    - Created README.md for Azure CLI commands to create a virtual network and subnets.
    - Added commands.sh for executing Azure CLI commands to set up the virtual network and subnets.
    - Introduced Terraform scripts including providers.tf for Azure provider configuration.
    - Added vnet.tf to define Azure resources for a virtual network and subnets.

---
+++ 1.Azure VNets/terraform/vnet.tf
@@ -0,0 +1,25 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-demo-vnet-001"
+  location = "westeurope"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-spoke"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet-frontend-servers" {
+  name                 = "subnet-frontend-servers"
+  resource_group_name  = azurerm_virtual_network.vnet.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_subnet" "subnet-backend-servers" {
+  name                 = "subnet-backend-servers"
+  resource_group_name  = azurerm_virtual_network.vnet.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
